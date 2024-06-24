92cafadebf89c54119f0d0824aacfbf5247996ea
ABT Aurelien <aurelien@abrassart.net>
2024-06-24 10:40:16 +0200

    Ajoutez des fichiers projet.

---
+++ Week4_Q_Dictionnaire/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Week4_Q_Dictionnaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Je veux que tu commences ton dictionnaire avec trois lettres: 'a', 'b' et 'c'.
+            // Je veux que tu associes trois mots à ces trois lettres.
+            Dictionary<char, string> dictionnaire = new Dictionary<char, string>();
+            dictionnaire.Add('a', "bonjour");
+            dictionnaire.Add('b', "à");
+            dictionnaire.Add('c', "tous");
+
+            // Affichage des mots du dictionnaire
+            foreach(string word in dictionnaire.Values)
+            {
+                Console.WriteLine(word);
+            }
+
+            // J'ai aussi besoin que tu me prouves que tu sais faire le ménage.
+            // Supprime-moi la lettre 'c' après l'avoir créée.
+            dictionnaire.Remove('c');
+
+            // Affichage des mots du dictionnaire
+            foreach (string word in dictionnaire.Values)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
